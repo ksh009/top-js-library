06551555fde2493839efb5e23251bd8a9ee62944
HughesSeanKyle <khughessean@yahoo.com>
2023-04-28 22:33:49 +0200

    Reset form values on form submit

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ class Book {
 	}
 }
 
-// Constructor can be modified to take a book in on initialization
 class Library {
 	constructor() {
 		this.books = [];
@@ -24,7 +23,7 @@ class Library {
 		const booksGrid = document.querySelector('.books-grid');
 		booksGrid.innerHTML = '';
 
-		this.books.forEach((book) => {
+		this.books.forEach((book, index) => {
 			// Create the necessary HTML elements
 			const bookCard = document.createElement('div');
 			const bookDetails = document.createElement('div');
@@ -34,6 +33,9 @@ class Library {
 			const bookLanguage = document.createElement('p');
 			const bookPublishDate = document.createElement('p');
 			const bookReadStatus = document.createElement('p');
+			const readLabel = document.createElement('label');
+			const readInput = document.createElement('input');
+			const deleteButton = document.createElement('button');
 
 			// Set the attributes and content of the elements
 			bookCard.className = 'book-card';
@@ -47,9 +49,14 @@ class Library {
 			bookLanguage.className = 'book-language';
 			bookLanguage.textContent = `Written in: ${book.language}`;
 			bookPublishDate.className = 'book-publish-date';
-			bookPublishDate.textContent = `Date published: ${book.dateOfPublish}`;
+			bookPublishDate.textContent = `Published: ${book.dateOfPublish}`;
 			bookReadStatus.className = 'book-read-status';
-			bookReadStatus.textContent = `Read it?: ${book.read ? 'Yes' : 'Not yet'}`;
+			readLabel.textContent = 'Read it?';
+			readInput.type = 'checkbox';
+			readInput.checked = book.read;
+			readLabel.appendChild(readInput);
+			bookReadStatus.appendChild(readLabel);
+			deleteButton.textContent = 'Remove';
 
 			// Append the elements to the selected element
 			bookDetails.appendChild(bookTitle);
@@ -58,36 +65,19 @@ class Library {
 			bookDetails.appendChild(bookLanguage);
 			bookDetails.appendChild(bookPublishDate);
 			bookDetails.appendChild(bookReadStatus);
+			bookDetails.appendChild(deleteButton);
 			bookCard.appendChild(bookDetails);
 			booksGrid.appendChild(bookCard);
-			// const bookDiv = document.createElement('div');
-			// bookDiv.classList.add('book');
 
-			// const titleDiv = document.createElement('div');
-			// titleDiv.textContent = book.title;
-			// bookDiv.appendChild(titleDiv);
-
-			// const authorDiv = document.createElement('div');
-			// authorDiv.textContent = book.author;
-			// bookDiv.appendChild(authorDiv);
-
-			// const pagesDiv = document.createElement('div');
-			// pagesDiv.textContent = book.pages;
-			// bookDiv.appendChild(pagesDiv);
-
-			// const lagnuageDiv = document.createElement('div');
-			// lagnuageDiv.textContent = book.pages;
-			// bookDiv.appendChild(lagnuageDiv);
-
-			// const dateOfPublishDiv = document.createElement('div');
-			// dateOfPublishDiv.textContent = book.pages;
-			// bookDiv.appendChild(dateOfPublishDiv);
-
-			// const readDiv = document.createElement('div');
-			// readDiv.textContent = book.read;
-			// bookDiv.appendChild(readDiv);
-
-			// booksGrid.appendChild(bookDiv);
+			// Event handlers
+			readInput.addEventListener('change', () => {
+				book.read = readInput.checked;
+				this.displayBooks();
+			});
+			deleteButton.addEventListener('click', () => {
+				this.books.splice(index, 1);
+				this.displayBooks();
+			});
 		});
 	}
 }
@@ -126,90 +116,7 @@ form.addEventListener('submit', (e) => {
 	library.addBook(title, author, pages, language, date, read);
 	library.displayBooks();
 
+	// Resets
+	form.reset();
 	modal.style.display = 'none';
 });
-
-// class Library {
-// 	constructor() {
-// 		this.books = [];
-// 	}
-
-// 	addBook(title, author, numPages, language, publishDate, isRead) {
-// 		const book = {
-// 			title: title,
-// 			author: author,
-// 			numPages: numPages,
-// 			language: language,
-// 			publishDate: publishDate,
-// 			isRead: isRead,
-// 		};
-// 		this.books.push(book);
-// 	}
-
-// 	updateBook(index, updatedBook) {
-// 		this.books[index] = { ...this.books[index], ...updatedBook };
-// 	}
-
-// 	deleteBook(index) {
-// 		this.books.splice(index, 1);
-// 	}
-
-// 	getBooks() {
-// 		return this.books;
-// 	}
-// }
-
-// const myLibrary = new Library();
-
-// // ADD/CREATE Books
-
-// console.log('Adding books...');
-
-// myLibrary.addBook(
-// 	'The Great Gatsby',
-// 	'F. Scott Fitzgerald',
-// 	180,
-// 	'English',
-// 	'April 10, 1925',
-// 	true
-// );
-// myLibrary.addBook(
-// 	'To Kill a Mockingbird',
-// 	'Harper Lee',
-// 	281,
-// 	'English',
-// 	'July 11, 1960',
-// 	false
-// );
-// myLibrary.addBook(
-// 	'The Lord of the Rings',
-// 	'Novel by J. R. R. Tolkien',
-// 	1222,
-// 	'English',
-// 	'July 29, 1954',
-// 	true
-// );
-
-// // Read Books
-// console.log(myLibrary.getBooks());
-
-// // Update Book
-// console.log('Updating book...');
-// myLibrary.updateBook(1, {
-// 	title: 'New Title',
-// 	numPages: 999,
-// });
-// myLibrary.updateBook(2, {
-// 	language: 'German',
-// 	author: 'unknown',
-// });
-
-// // Read Books
-// console.log(myLibrary.getBooks());
-
-// // Delete Book
-// console.log('Deleting book...');
-// myLibrary.deleteBook(0);
-
-// // Read Books
-// console.log(myLibrary.getBooks());
